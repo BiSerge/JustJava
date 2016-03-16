e6154244441a279ff14efb35d9f649825a1a0a8c
Сергей <bichevoi@gmail.com>
2016-03-16 23:11:15 +0600

    добавил метод calculatePrice displayPrice

--- app/src/main/java/com/example/biche/justjava/MainActivity.java
+++ app/src/main/java/com/example/biche/justjava/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number) + "\n Thank you!");
     }
 
     /**
